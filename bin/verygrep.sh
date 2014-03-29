360f644a43ac60266d14f6435e51ce41d4ce4baa
mk-pmb <mk-pmb@users.noreply.github.com>
2014-03-29 17:07:36 +0100

    verygrep: remove some backslash-escapes from dictionary words

--- bin/verygrep.sh
+++ bin/verygrep.sh
@@ -6,8 +6,15 @@ SELFPATH="$(readlink -m "$0"/..)"
 function main () {
   local DICTS=( "$@" )
   [ -n "${DICTS[*]}" ] || DICTS=( "$SELFPATH"/* )
-  local WORDS=( $(grep "${DICTS[@]}" -hoPie '[a-z][a-z0-9_]{2,20}[a-z]' \
-    | tr 'A-Z_' 'a-z ' | sort -u) )
+  local WORDS=( $(grep "${DICTS[@]}" -hoPie '(^|.)[a-z][a-z0-9_]{2,20}[a-z]' \
+    | sed -re '
+      s~^\\x[0-9a-fA-F]{2}~ ~
+      s~^\\u[0-9a-fA-F]{4}~ ~
+      s~^\\U[0-9a-fA-F]{8}~ ~
+      s~^\\[a-z]~ ~
+      s~^[^A-Za-z ]~~    # incl. &html;
+      s~^ ~~
+    ' | tr 'A-Z_' 'a-z ' | sort -u) )
   exec nodejs "$SELFPATH"/such-cmd.js "${WORDS[@]}"
 }
 
